b3a8df8438a7a61cd25d50ecc908a0b49bf475a0
Monet <104532694+mpfuns@users.noreply.github.com>
2023-01-26 19:48:40 +0000

    working  on still

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -2,11 +2,26 @@ import React, { useState } from 'react';
 export default Sidebar
 function Sidebar() {
   
+  const [thingArray, setThingArray] = React.useState(["thing1","thing2"])
    
+  function addForm(){
+
+
+    setThingArray(prev => [...prev, `thing${prev.length + 1}`])
+    return console.log(thingArray)
+   }
+
+function delForm(){
+
+
+  return console.log(thingArray)
+}
+
 
   return (
     <div className="sidebar">
-      sidebar here 
+      <button onClick={addForm} className='main-plus-sidebar'><i class="fa-solid fa-plus"></i></button>
+      <button onClick={delForm} className='main-trash-sidebar'><i class="fa-solid fa-trash"></i></button>
     </div>
   );
 }
