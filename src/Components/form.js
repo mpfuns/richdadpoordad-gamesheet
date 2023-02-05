583d800b2e51afb2b33de79fa82f6ffcc1e4a6ba
Monet <104532694+mpfuns@users.noreply.github.com>
2023-02-05 21:35:48 +0000

    finish  button display

--- src/Components/form.js
+++ src/Components/form.js
@@ -13,8 +13,13 @@ function Form() {
                 type="number"
                 placeholder="0"
                 //onChange={handleChange}
-                id="salaray"
+                id="salary"
             />
+            <div id="interest">
+            <h3>Interest/dividends:</h3>
+            <button><i class="fa-solid fa-plus"></i></button>
+            <button><i class="fa-solid fa-trash"></i></button>
+            </div>
     </div>
     <div className='Expenses'>
       <h2>Expenses</h2>
@@ -25,7 +30,7 @@ function Form() {
                 //onChange={handleChange}
                 id="Taxes"
             />
-            <label htmlFor="Home-e">Home Mortage: $</label>
+            <label htmlFor="Home-e">Home Mortgage: $</label>
       <input
                 type="number"
                 placeholder="0"
@@ -67,6 +72,11 @@ function Form() {
                 //onChange={handleChange}
                 id="Childern"
             />
+            <div id="other-expense">
+            <h3>Other Expense:</h3>
+            <button><i class="fa-solid fa-plus"></i></button>
+            <button><i class="fa-solid fa-trash"></i></button>
+            </div>
     </div>
     <div className='Asset'>
       <h2>Asset</h2>
@@ -84,10 +94,20 @@ function Form() {
                 //onChange={handleChange}
                 id="Precious"
             />
+            <div id="three-share">
+            <h3>Stocks/Funds/CDS: # of Share:  Cost/Shares:</h3>
+            <button><i class="fa-solid fa-plus"></i></button>
+            <button><i class="fa-solid fa-trash"></i></button>
+            </div>
+            <div id="three-real">
+            <h3>Real Estate/Business: Down Payment: Cost:</h3>
+            <button><i class="fa-solid fa-plus"></i></button>
+            <button><i class="fa-solid fa-trash"></i></button>
+            </div>
     </div>
     <div className='Libilites'>
       <h2>Libilites</h2>
-      <label htmlFor="Home-l">Home Mortage: $</label>
+      <label htmlFor="Home-l">Home Mortgage: $</label>
       <input
                 type="number"
                 placeholder="0"
@@ -122,6 +142,11 @@ function Form() {
                 //onChange={handleChange}
                 id="bank-l"
             />
+            <div id="two-real-mort">
+            <h3>Real Estate/Business:  Mortgage/Liability:</h3>
+            <button><i class="fa-solid fa-plus"></i></button>
+            <button><i class="fa-solid fa-trash"></i></button>
+            </div>
     </div>
     <div className='math-section'>
       <h2>Math</h2>
